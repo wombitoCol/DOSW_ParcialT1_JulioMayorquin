235bc8ef09623cdd714c92c11de18ab9a853a277
wombito <wombito@localhost.localdomain>
2026-02-27 20:19:27 -0500

    estructura maven

---
+++ DOSW-ParcialT1/src/main/java/edu/dosw/parcial/DOSW_ParcialT1/DoswParcialT1Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial.DOSW_ParcialT1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswParcialT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswParcialT1Application.class, args);
+	}
+
+}
